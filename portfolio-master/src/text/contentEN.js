02d6f4c4ffd9ebff89e30fa7af8013cf2d6d16d2
unknown <sumedh.dawadi101@gmail.com>
2024-06-12 07:45:13 +0545

    website-deployed

---
+++ portfolio-master/src/text/contentEN.js
@@ -0,0 +1,163 @@
+
+
+var contentEN = {
+  "aboutme" : {
+    "name" : "About me",
+    "content" : 
+<>
+<p>
+ Greetings ♥ I'm <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/sumedhdawadi/">Sumedh</a>,<br/>
+ As a security analyst with 3+ Years of experience in a Security Operation Center, I am passionate about Cyber Security and have a successful track record in Incident Response, Malware Analysis, Digital Forensics, and Network Penetration Testing.In addition to my technical
+skills, I am a highly analytical and detail-oriented individual with strong problem-solving abilities. I
+am confident in my ability to identify and mitigate potential security threats and to develop and
+implement effective security measures. I am excited to bring my skills and experience to a new
+opportunity and to continue learning and growing in the field of cyber security.
+</p>
+<p>
+<h3>EXPERIENCES:</h3>
+<b>💼CryptoGen Nepal Pvt. Ltd.</b>
+<ul>
+  <li><b>Role   :</b> Associate Defensive Security Analyst</li>
+  <li><b>Tenure :</b> 13th February 2022 - 16th September 2022</li>
+</ul>
+<b>Accomplishment</b>
+<ul>
+  <li>Hands-on with Enterprise and Open-Source <b>SIEM</b> Solutions. </li>
+  <li> Knowledge on SOC Analyst workflow and procedure.</li>
+  <li> Experience on Real-time SOC monitoring during Projects.</li>
+  <li> Experience on Practical implementation of Digital Forensics for Projects.</li>
+  <li> Experience on Pratical implementation of Incident Response for Projects.</li>
+</ul>  
+</p>
+<p>
+<h4>💼Muktinath Bikas Bank Limited.</h4>
+<ul>
+  <li><b>Role   :</b> Information Security Analyst.</li>
+  <li><b>Tenure :</b> 18th September 2022 - 20th October 2023.</li>
+</ul>
+<b>Roles and Responsiblities:</b>
+<ul>
+  <li>Real Time SIEM Monitoring.</li>
+  <li>Scanning and monitoring computers network systems and applications for security vulnerabilities to anticipate and protect against potential threats. </li>
+  <li>Investigating security breaches and other cybersecurity incident. </li>
+  <li>Deploying security measures.</li>
+  <li>Perform Vulnerability Assessment and Penetration Testing.</li>
+  <li>Analyzing IT requirements and providing objective advice on the use of IT security requirement. </li>
+  <li>Conducting internal security assessment </li>
+  <li>Conducting POC's in timely manner. </li>
+  <li>Gather feedback from end-user to continue improving systems. </li>
+  <li>Regular briefing of the task to the Line Manager</li>
+
+</ul> 
+</p>
+
+<h4>💼StickmanCyber</h4>
+<ul>
+  <li><b>Role   :</b> security operation center (SOC) L2 Engineer</li>
+  <li><b>Tenure :</b> 13th November 2023 - 20th March 2024.</li>
+</ul>
+<b>Roles and Responsiblities:</b>
+<ul>
+<li> Monitor SIEM Platforms, EDR's, analyse, and resolve network security device, systems, and tools issues </li>
+<li> Follow both work instruction playbooks and runbook automations activities. Provide feedback
+to modify and suggest changes to work instructions for similar impacting events  </li>
+<li>Understand and work within established Service Level Agreements (SLA) to ensure timely response</li>
+<li>Support and promote the company's Cybersecurity operation standards and excellence.</li>
+<li>Continuous improvement of core functions expected from Cybersecurity Operations centre.</li>
+<li>Track, update and resolve all assigned incidents, changes, and problem reports in the incident management system, ensuring that documentation is thorough, accurate and meets a standard of high quality</li>
+<li>Understand internal tools and processes and leverage those to drive automation and efficiencies.</li>
+<li>Locate trends in abuse vectors, communicate with leadership, and advocate for appropriate change to prevent future occurrences.</li>
+<li>Follow documented support procedures, managing each issue through resolution or turnover to maintain established service levels</li>
+<li>Ability to follow and update established and/or ad-hoc processes and work instructions and create procedures where deficiencies are identified.</li>
+<li>Proactively monitor, recognize, analyze, isolate and/or resolve documented hardware and software problems utilizing a variety of hardware and software testing techniques</li>
+<li>Articulate problem statements with clarity. Consult with other analysts and support groups or development teams to drive toward long-term restoration and resolution of incidents</li>
+<li>Responsible for the internal and external communication of issues to management, other internal support groups, and customer pages, email broadcasts, or verified phone calls.</li>
+</ul>
+
+
+
+<p>
+I am excited to bring my skills and experience to a new opportunity and to continue learning and growing in the field of cyber security.
+</p>
+</>
+  },
+
+
+  "skills" : {
+    "name" : "Skills", 
+    "content" : 
+<>
+<p>
+
+<b>SKILLS: </b>
+<ul>
+  
+  <li>SIEM (Security Information and Event Management) including Google Chronicle , Threat Defence, LogPoint and Rapid7.</li>
+  <li>Hands on EDR's such as Crowdstrike falcon and  Trend Micro XDR </li>
+  <li>Incident Response (IR).</li>
+  <li>Digital Forensics.</li>
+  <li>Threat Hunting.</li>
+  <li>Malware Analysis.</li>
+  <li>Tune Rules for better detection.</li>
+</ul> 
+</p>
+
+<h4>KNOWLEDGES: </h4>
+<ul>
+  <li>Purple Team Exercise with Capture The Flag (CTF).</li>
+  <li>Bash Scriptig for building security tools.</li>
+  <li>Attack Simulation with Atomic Red Team and MITRE CALDERA .</li>
+  <li>Creating playbooks to automate SOC workflow</li>
+  <li>Comfortable working on YALA Rules and Yara L for detection</li>
+  <li>Create Dahsboard for detection visualization</li>
+  
+</ul>
+
+<h4>I have tons of research on offensive security as well as defensive security. You can find it below :  </h4>
+<li><a rel="noreferrer" target="_blank" href="https://sumedh00.medium.com/">Research.</a></li>
+
+</>
+  },
+  "achievements" : {
+    "name" : "Achievement",
+    "content" : 
+<>
+<b>CERTIFICATES:</b>
+<ul>
+
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1ysZV1gaFvjNOwdXInjgMXBLTXbNGLxQ7/view?usp=sharing">Google Chronicle SOAR Fundamental</a></li>
+    <li><a rel="noreferrer" target="_blank" href="https://codered.eccouncil.org/certificate/25371246-43f3-4a04-9e85-66a97fe46d65?logged=false">Ethical Hacking Essentials.</a>  </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1wxlIrvlfP1vWMdHxXhBMjGiyt57v95Wo/view">Network Defense Essentials.</a> </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1BMKjuHLhD2gV3TTJUILM4nXl7QH91hc7/view?pli=1">Practical Web Application Security and Testing</a> </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1U6ukRzEjBz0PG3mq1gK-B2hR5AKz46I4/view">Breach and Attack Simulation</a> </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1U6ukRzEjBz0PG3mq1gK-B2hR5AKz46I4/view">Cysa+ (CS0-002) By Cybrary</a> </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1RR6HaaVnfZXsCy1pHDaYvdLIm4NM4QNz/view">Introduction to Vulnerability Management.</a></li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1Nn6jogUCjGB3ZqL72BnhEsWnwrpe08-X/view">Introduction to Dark Web Operations.</a></li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1aHDWL-8f83ECYQzj2-DXSomT_F82d1gd/view">NSE 1 Network Security Associate.</a> </li>
+    
+</ul>
+<b>HALL OF FAME : </b>
+<ul>
+  <li><a rel="noreferrer" target="_blank" href="https://support.apple.com/en-us/HT201536">Acknowledged by Apple Inc.</a></li>
+  <li><a rel="noreferrer" target="_blank" href="https://www.surf.nl/en/hall-of-fame-0">Acknowledge by Surf.nl.</a></li>
+  <li><a rel="noreferrer" target="_blank" href="">Acknowledge by Softwarica College of IT and E-commerce.</a></li>
+</ul>
+</>
+  },
+
+  "contact" : {
+    "name" : "Contact",
+    "content" : 
+<>
+<p>I am always open to discussing opportunities and am eager to contribute my skills and abilities to a team. Please feel free to contact me at your convenience.</p>
+
+<p> 📌 <a rel="noreferrer" target="_blank" href="https://github.com/SumedhDawadi">Github</a></p>
+<p> 💡 <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/sumedhdawadi/">Linkedin</a></p>
+<p> ✎ <a rel="noreferrer" target="_blank" href="https://medium.com/@sumedh00">Medium</a></p>
+<p> 📨 <a href="mailto:pingsumedh@gmail.com">pingsumedh@gmail.com</a></p>
+</>
+  }
+}
+
+
+export default contentEN
